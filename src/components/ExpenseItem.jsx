3e1fbc08ec6bdcada08d635e2ea28ac2c64aa5c0
mdshayequeahmad <shayeque01@gmail.com>
2023-05-04 19:09:24 +0530

    passing data via props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return (
-        <div>
-            <h1>Expense Items</h1>
-            <h2>Food Rs 10</h2>
-            <h2>Petrol Rs 100</h2>
-            <h2>Movies Rs 200</h2>
+        <div className='expense-item'>
+            <div>{props.date[0]}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <h2>{props.LocationOfExpenditure}</h2>
+                <div className='expense-item__price'> $ {props.amount}</div>
+            </div>
         </div>
     )
 }
