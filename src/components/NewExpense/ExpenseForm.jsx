b98dda065bf9b41338e2fbb21323bae16e384e0f
mdshayequeahmad <shayeque01@gmail.com>
2023-05-11 00:10:22 +0530

    Challenges on Conditional Rendering

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -72,6 +72,7 @@ const ExpenseForm = (props) => {
                 <input type='date' value={enteredDate} min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler} />
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </div>
