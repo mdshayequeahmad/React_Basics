0e445a557d85406dc5e80b46e062d13bcd62adce
mdshayequeahmad <shayeque01@gmail.com>
2023-05-11 01:40:27 +0530

    Chart App

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -46,9 +46,9 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        }
+            amount: +enteredAmount,
+            date: new Date(enteredDate),
+          };
 
         props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
